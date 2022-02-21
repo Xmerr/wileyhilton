e7d9174e8438c2dfd3bc06e441274030bb5d6083
Xmer <wileyhilton@gmail.com>
2022-02-21 20:24:09 +0000

    init

---
+++ src/helpers/Theme.jsx
@@ -0,0 +1,37 @@
+import useMediaQuery from '@mui/material/useMediaQuery';
+import CssBaseline from '@mui/material/CssBaseline';
+import React, { useMemo } from 'react';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material';
+
+import blue from '@mui/material/colors/blue';
+import purple from '@mui/material/colors/purple';
+
+export const Theme = ({ children }) => {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const theme = useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? 'dark' : 'light',
+                    primary: purple,
+                    secondary: blue,
+                    text: {
+                        primary: '#fff',
+                        secondary: blue[500],
+                    },
+                },
+            }),
+        [prefersDarkMode]
+    );
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+        </ThemeProvider>
+    );
+};
+
+export default Theme;
